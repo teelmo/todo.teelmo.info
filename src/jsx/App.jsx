1847136ffe0a0ec99f704b2f97a304ad68381831
Teemo Tebest <teemo.tebest@gmail.com>
2021-08-03 15:54:53 +0200

    Improved styles on mobile

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -31,20 +31,20 @@ class App extends Component {
     return (
       <div>
         <div className={style.app}>
-          <h1>With Teemo…</h1>
-          <label><input type="checkbox" onClick={() => this.onClick()} disabled={true} checked={true} /> Floating down the Rhone</label>
-          <label><input type="checkbox" onClick={() => this.onClick()} disabled={true} /> Museum day in Geneva</label>
-          <label><input type="checkbox" onClick={() => this.onClick()} disabled={true} /> Free skiing down a mountain</label>
-          <label><input type="checkbox" onClick={() => this.onClick()} disabled={true} /> Over nighter at a ski resort</label>
-          <label><input type="checkbox" onClick={() => this.onClick()} disabled={true} /> Ping pong tournament</label>
-          <label><input type="checkbox" onClick={() => this.onClick()} disabled={true} /> Bains des Pâquis sauna</label>
-          <label><input type="checkbox" onClick={() => this.onClick()} disabled={true} /> Hike to Mont Tendre</label>
-          <label><input type="checkbox" onClick={() => this.onClick()} disabled={true} /> Tour de Lac Leman</label>
-          <label><input type="checkbox" onClick={() => this.onClick()} disabled={true} /> Course de l'Escalade</label>
-          <label><input type="checkbox" onClick={() => this.onClick()} disabled={true} /> Beach Volley at Vessey</label>
-          <label><input type="checkbox" onClick={() => this.onClick()} disabled={true} /> Cycle up Salève</label>
-          <label><input type="checkbox" onClick={() => this.onClick()} disabled={true} /> Day by the lake w/ friends</label>
-          <label><input type="checkbox" onClick={() => this.onClick()} disabled={true} /> Drink at Grottes Market</label>
+          <h1>Todo w/ Teemo…</h1>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} checked={true} /></span> <span className={style.label}>Floating down the Rhone</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Museum day in Geneva</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Free skiing down a mountain</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Over nighter at a ski resort</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Ping pong tournament</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Bains des Pâquis sauna</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Hike to Mont Tendre</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Tour de Lac Leman</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Course de l'Escalade</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Beach Volley at Vessey</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Cycle up Salève</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Day by the lake w/ friends</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Drink at Grottes Market</span></label>
         </div>
         <div className={style.background}></div>
       </div>
