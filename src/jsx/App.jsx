ad3b494fa8b298af68ee14f182172a4b3e3ac5bd
Teemo Tebest <teemo.tebest@gmail.com>
2021-09-04 20:26:43 +0200

    Updated sub title

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -30,7 +30,7 @@ class App extends Component {
       <div>
         <div className={style.app}>
           <h1>Todo w/ Teemo…</h1>
-          <p>Things to do with Teemo while he is still in Geneva, be active and suggest an activity from below or come up with your own!</p>
+          <p>Things to do with Teemo, be active and suggest an activity from below or come up with your own!</p>
           <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} checked={true} /></span> <span className={style.label}>Floating down the Rhone</span></label>
           <div className={style.done_container}>Thx Jan & et al. ❤️❤️❤️ Happy to go again!</div>
           <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Museum day in Geneva</span></label>
