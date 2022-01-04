0618b3414d383d27763e5628ab8f47a5366698e6
Teemo Tebest <teemo.tebest@gmail.com>
2022-01-04 16:22:21 +0100

    Updated list

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -35,18 +35,20 @@ class App extends Component {
           <div className={style.done_container}>Thx Jan & et al. ❤️❤️❤️ Happy to go again!</div>
           <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Museum day in Geneva</span></label>
           <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Free skiing down a mountain</span></label>
-          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Over nighter at a ski resort</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} checked={true}/></span> <span className={style.label}>Over nighter at a ski resort</span></label>
+          <div className={style.done_container}>Thx Helena & Fanny!</div>
           <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Ping pong tournament</span></label>
           <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Bains des Pâquis sauna</span></label>
           <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Hike to Mont Tendre</span></label>
           <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} checked={true}/></span> <span className={style.label}>Tour de Lac Leman</span></label>
           <div className={style.done_container}>Thx Dante 🚲💦🚲💦!</div>
-          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Course de l'Escalade</span></label>
+          <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} checked={true}/></span> <span className={style.label}>Course de l'Escalade</span></label>
+          <div className={style.done_container}>A new record of 35' 15'' 1!</div>
           <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Beach Volley at Vessey</span></label>
           <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Cycle up Salève</span></label>
           <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Day by the lake w/ friends</span></label>
           <label><span className={style.input_container}><input type="checkbox" onClick={() => this.onClick()} disabled={true} /></span> <span className={style.label}>Drink at Grottes Market</span></label>
-          <label className={style.extra}><span className={style.input_container}></span> <span className={style.label}>+ Hikes, Badminton, Volleyball,  Running, Tabletennis and Everything!</span></label>
+          <label className={style.extra}><span className={style.input_container}></span> <span className={style.label}>+ Hikes, Badminton, Volleyball, Running, Tabletennis and Everything!</span></label>
           <p><strong>Note:</strong> none are related to food ;)</p>
         </div>
         <div className={style.background}></div>
